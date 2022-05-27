666e58af3dbace89d4524c75ec318e71ab37f7f1
Abdur Rahman Suzon <abdur.rahman@egeneration.co>
2022-05-27 10:56:35 +0600

    Create order

--- src/pages/Dashboard/AddProduct/AddProduct.js
+++ src/pages/Dashboard/AddProduct/AddProduct.js
@@ -1,95 +1,99 @@
-import React from 'react';
-import { Col, Container, Form, Row } from 'react-bootstrap';
-import { useForm } from 'react-hook-form';
-import { toast } from 'react-toastify';
-const axios = require('axios').default;
+import React from "react";
+import { Col, Container, Form, Row } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+const axios = require("axios").default;
 
 const AddProduct = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = (data, event) => {
-        console.log(data);
-        axios.post('http://localhost:5000/product', data)
-        .then(function (response) {
-          // console.log(response);
-          toast("Product added successfully!");
-          event.target.reset();
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    };
-    return (
-    <div className='page-block h-100 d-flex'>
-         <Container>
-          <Row>
-            <Col>
-            <form onSubmit={handleSubmit(onSubmit)}>
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data, event) => {
   
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>Product Name</Form.Label>
-                  <input
-                    className="form-control"
-                    {...register("productName", { 
-                      required: true,
-                     })}
-                  />
-     </Form.Group>
-   
-      
-     <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>Product Price</Form.Label>
-                  <input
-                    className="form-control"
-                    {...register("productPrice", { 
-                      required: true,
-                     })}
-                  />
-     </Form.Group>
-     <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>Product Description</Form.Label>
-                  <input
-                    className="form-control"
-                    {...register("productDescription", { 
-                      required: true,
-                     })}
-                  />
-     </Form.Group>
-     <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>Product image</Form.Label>
-                  <input
-                    className="form-control"
-                    {...register("productPhoto", { 
-                      required: true,
-                     })}
-                  />
-     </Form.Group>
-     <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>Minimum order quantity</Form.Label>
-                  <input
-                    className="form-control"
-                    {...register("minimumQuantity", { 
-                      required: true,
-                     })}
-                  />
-     </Form.Group>
+    axios
+      .post("http://localhost:5000/product", data)
+      .then(function (response) {
+        // console.log(response);
+        toast("Product added successfully!");
+        event.target.reset();
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+  return (
+    <div className="page-block h-100 d-flex">
+      <Container>
+        <Row>
+          <Col>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Product Name</Form.Label>
+                <input
+                  className="form-control"
+                  {...register("productName", {
+                    required: true,
+                  })}
+                />
+              </Form.Group>
 
-     <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>Available quantity</Form.Label>
-                  <input
-                    className="form-control"
-                    {...register("availableQuantity", { 
-                      required: true,
-                     })}
-                  />
-     </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Product Price</Form.Label>
+                <input
+                  className="form-control"
+                  {...register("productPrice", {
+                    required: true,
+                  })}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Product Description</Form.Label>
+                <input
+                  className="form-control"
+                  {...register("productDescription", {
+                    required: true,
+                  })}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Product image</Form.Label>
+                <input
+                  className="form-control"
+                  {...register("productPhoto", {
+                    required: true,
+                  })}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Minimum order quantity</Form.Label>
+                <input
+                  className="form-control"
+                  {...register("minimumQuantity", {
+                    required: true,
+                  })}
+                />
+              </Form.Group>
 
-      <input type="submit" />
-    </form>
-            </Col>
-          </Row>
-        </Container>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Available quantity</Form.Label>
+                <input
+                  className="form-control"
+                  {...register("availableQuantity", {
+                    required: true,
+                  })}
+                />
+              </Form.Group>
+
+              <input type="submit" />
+            </form>
+          </Col>
+        </Row>
+      </Container>
     </div>
-    );
+  );
 };
 
 export default AddProduct;
--- src/pages/Purchase/Purchase.js
+++ src/pages/Purchase/Purchase.js
@@ -1,13 +1,25 @@
-import { Button } from "bootstrap";
 import React, { useEffect, useState } from "react";
-import { Col, Container, InputGroup, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, FormControl, InputGroup, Row } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 import { Link, useParams } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import { toast } from "react-toastify";
 
 const Purchase = () => {
+ const [user] = useAuthState(auth);
   const axios = require("axios").default;
   const { id } = useParams();
   //   console.log(id);
   const [product, setProduct] = useState({});
+  const [orderQty, setOrderQty] = useState(0);
+  const [maxOrderQty, setMaxOrderQty] = useState(0);
+  const [minOrderQty, setMinOrderQty] = useState(0);
+  useEffect(()=>{
+    setOrderQty(parseInt(minimumQuantity));
+    setMinOrderQty(parseInt(minimumQuantity));
+    setMaxOrderQty(parseInt(availableQuantity)); 
+  }, [product])
   const {
     _id,
     productName,
@@ -17,11 +29,60 @@ const Purchase = () => {
     availableQuantity,
     minimumQuantity,
   } = product;
+
+  const increaseQty = () =>{
+    const newQty = parseInt(orderQty)  + 1; 
+    if(newQty <= maxOrderQty){ 
+        console.log(newQty);
+        setOrderQty(newQty)
+    } else{
+        alert("Stop!!")
+    }
+}
+  const decreaseQty = () =>{
+    const newQty = parseInt(orderQty)  - 1; 
+    if(newQty >= minOrderQty){ 
+        console.log(newQty);
+        setOrderQty(newQty)
+    } else{
+        alert("Stop!!")
+    }
+}
   useEffect(() => {
     axios.get(`http://localhost:5000/product/${id}`).then(function (response) {
       setProduct(response.data);
+     
     });
   }, []);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (userInput, event) => {
+    // console.log(userInput);
+    const orderSummary = {
+        ...userInput, 
+        productID : _id,
+        productName, 
+        productPrice,
+        orderQuantity: orderQty,
+        productPhoto,  
+    }
+    console.log(orderSummary);
+    axios.post('http://localhost:5000/order', orderSummary)
+    .then(function (response) {
+      // console.log(response);
+      toast("Order Places successfully!");
+      event.target.reset();
+      setOrderQty(minimumQuantity);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
   return (
     <div className="page-block">
       <Container>
@@ -39,30 +100,69 @@ const Purchase = () => {
               Min Order Quantity : {minimumQuantity}{" "}
             </p>
             {/* <Button onClick={delivered}>Delivered</Button> */}
-            {/* <form className="add-stock-form" onSubmit={addStock}>
+             
+                
               <InputGroup className="my-5">
-                <FormControl
-                  placeholder="Add stock"
-                  aria-label="Add stock"
-                  aria-describedby="basic-addon2"
-                  name="stock"
-                  type="number"
-                  required
+              <Button onClick={decreaseQty} variant="secondary" id="button-addon2">
+                 -
+                </Button>
+                <FormControl 
+                  type="number"  
+                  value={orderQty}
+                  readOnly 
                 />
-                <Button type="submit" variant="secondary" id="button-addon2">
-                  Add Stock
+                <Button onClick={increaseQty} variant="secondary" id="button-addon2">
+                 +
                 </Button>
               </InputGroup>
-            </form> */}
+           
             <Link className="btn btn-info" to="/manage-inventories">
               {" "}
               Manage Inventories{" "}
             </Link>
           </Col>
         </Row>
-        <Row>
-          <Col>
-          
+        <Row className="justify-center">
+        <Col xs={12} sm={6} md={6}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Name</Form.Label>
+                <input defaultValue={user.displayName}
+                  className="form-control"
+                  {...register( "customerName", {
+                    required: true,
+                  })}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email</Form.Label>
+                <input defaultValue={user.email}
+                  className="form-control"
+                  {...register( "customerEmail", {
+                    required: true,
+                  })}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Phone</Form.Label>
+                <input 
+                  className="form-control"
+                  {...register( "customerPhone", {
+                    required: true,
+                  })}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Address</Form.Label>
+                <input 
+                  className="form-control"
+                  {...register( "customerAddress", {
+                    required: true,
+                  })}
+                />
+              </Form.Group>
+              <input type="submit" value="Purchase" />
+            </form>
           </Col>
         </Row>
       </Container>

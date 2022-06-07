f3a162a8b9d503e4011f2ff8cc69cc7869b1a84e
Abdur Rahman Suzon <abdur.rahman@egeneration.co>
2022-06-07 12:55:47 +0600

    manage top nav

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,49 +1,75 @@
 import { signOut } from "firebase/auth";
 import React from "react";
-import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import {
+  Button,
+  Col,
+  Container,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Row,
+} from "react-bootstrap";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
 import auth from "../firebase.init";
+import logo from "../images/logo.png";
 
 const Header = () => {
   const [user] = useAuthState(auth);
   return (
-    <Navbar bg="primary" variant="dark" expand="lg">
+    <nav className="custom-navbar">
       <Container>
-        <Navbar.Brand href="#">Gigabite</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <Link className="nav-link" to="/">
-              Home
-            </Link>
-            <Link className="nav-link" to="/blog">
-              Blog
-            </Link>
-            <Link className="nav-link" to="/portfolio">
-              My Portfolio
-            </Link>
-
-            <Link className="nav-link" to="/dashboard">
-              Dashboard
-            </Link>
-          </Nav>
-          {user ? (
-            <Button onClick={() => signOut(auth)} variant="outline-light">
-              Hi,{user.displayName} Logout
-            </Button>
-          ) : (
-            <Link className="btn btn-outline-light" to="/login">
-              Login
-            </Link>
-          )}
-        </Navbar.Collapse>
+        <div className="navbar-inner">
+          <div className="logo">
+            <img src={logo} alt="" />
+          </div>
+          <div className="menu-bar">
+            <ul>
+              <li>
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/blog">
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/portfolio">
+                  My Portfolio
+                </Link>
+              </li>
+              {user ? (
+                <li>
+                  <Link className="nav-link" to="#">
+                    Hi,{user.displayName}
+                  </Link>
+                  <ul>
+                    <li> 
+                      <a href="#" onClick={() => signOut(auth)}> Signout </a>
+                    </li>
+                    <li>
+                      <Link className="nav-link" to="/dashboard">
+                        Dashboard
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+              ) : (
+                <li>
+                  {" "}
+                  <Link className="nav-link" to="/login">
+                    Login
+                  </Link>
+                </li>
+              )}
+              <li></li>
+            </ul>
+          </div>
+        </div>
       </Container>
-    </Navbar>
+    </nav>
   );
 };
 
